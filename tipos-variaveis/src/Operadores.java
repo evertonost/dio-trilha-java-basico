89923dbc278f6663b610996aab61759b327fccdf
Everton Ostrufka <ost.everton@gmail.com>
2024-05-10 09:15:56 -0300

    updete operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,60 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String concatenacao ="?";
+        int numero = 5;
+        numero = - numero;
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" +( 1 + 1 + 1);
+
+        System.out.println(concatenacao);   
+
+       System.out.println(- numero); 
+       System.out.println( numero); 
+
+       numero = +numero;
+
+       System.out.println( numero*-1); 
+
+       numero ++ ;
+
+       System.out.println( numero); 
+
+       numero = numero + 2 ;
+       System.out.println( numero); 
+
+       numero -- ;
+
+       System.out.println( numero); 
+
+       numero = numero - 2 ;
+
+       System.out.println( numero); 
+
+
+
+
+
+
+
+    }
+
+    
+
+
+}
